3685a61be9fb6a0206bea5028e354dfd90fb9706
Nilodae <nilode.flanker44@gmail.com>
2026-06-02 21:22:36 +0700

    pysniff UI

---
+++ appInterface.py
@@ -0,0 +1,8 @@
+import PySide6.QtCore
+import sys
+import random
+from PySide6 import QtCore, QtWidgets, QtGui
+
+print(PySide6.__version__)
+print(PySide6.QtCore.__version__)
+
--- packetSniff.py
+++ packetSniff.py
@@ -1,6 +1,9 @@
 import sys
 from scapy.all import sr1,IP,ICMP
 
-p=sr1(IP(dst=sys.argv[1])/ICMP())
+input_dst = input("Please enter an IP address: ")
+
+p=sr1(IP(dst=input_dst)/ICMP())
 if p:
     p.show()
+
