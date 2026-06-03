cc6a5d1e1af0d2500af4a4aa6386b528c195d5f2
Nilodae <nilode.flanker44@gmail.com>
2026-06-03 23:13:54 +0700

    still got work done ig

--- appInterface.py
+++ appInterface.py
@@ -1,8 +1,34 @@
-import PySide6.QtCore
 import sys
-import random
-from PySide6 import QtCore, QtWidgets, QtGui
+from PySide6.QtWidgets import (QLineEdit, QLabel, QPushButton, QApplication,
+    QVBoxLayout, QDialog)
 
-print(PySide6.__version__)
-print(PySide6.QtCore.__version__)
 
+class MainWidget(QDialog):
+    def __init__(self, parent=None):
+        super(MainWidget, self).__init__(parent)
+        self.edit = QLineEdit()
+        self.button = QPushButton("Confirm")
+        self.title = QLabel("Minh's Packet Sniffer Application")
+        self.IPtext = QLabel("Enter the IP Address: ")
+        layout = QVBoxLayout()
+        layout.addWidget(self.title)
+        layout.addWidget(self.IPtext)
+        layout.addWidget(self.edit)
+        layout.addWidget(self.button)
+        self.setLayout(layout)
+        self.button.clicked.connect(self.build_packet)
+
+
+
+    def build_packet(self):
+        print(f"Your IP Address is: {self.edit.text()}")
+    
+    
+
+if __name__ == "__main__":
+    app = QApplication([])
+    widget = MainWidget()
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec())
