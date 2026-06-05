76865250029eb2877c3918d57877f9c79fdbbf04
Nilodae <nilode.flanker44@gmail.com>
2026-06-05 22:51:29 +0700

    TS CAN SNIFF WTF

--- appInterface.py
+++ appInterface.py
@@ -8,27 +8,47 @@ from PySide6.QtWidgets import (QLineEdit, QLabel, QPushButton, QApplication,
 class MainWidget(QDialog):
     def __init__(self, parent=None):
         super(MainWidget, self).__init__(parent)
-        self.edit = QLineEdit()
-        self.edit1 = QLineEdit()
-        self.button = QPushButton("Confirm")
+        self.FieldIPAddress = QLineEdit()
+        self.FieldDuration = QLineEdit()
+        self.FieldPacketAmount = QLineEdit()
+        self.FieldVariableName = QLineEdit()
+        self.button = QPushButton("Send Echo Request")
+        self.button1 = QPushButton("Sniff!")
         self.title = QLabel("Minh's Packet Sniffer Application")
-        self.IPtext = QLabel("Enter the IP Address: ")
-        self.IPtext1 = QLabel("Enter the duration: ")
+        self.title1 = QLabel("ICMP Echo Request")
+        self.title2 = QLabel("Sniff Incoming Traffic")
+        self.Fieldtext = QLabel("Enter the IP Address: ")
+        self.Fieldtext1 = QLabel("Enter the duration: ")
+        self.Fieldtext2 = QLabel("Enter the amount of packets")
+        self.Fieldtext3 = QLabel("Enter the variable name: ")
         layout = QVBoxLayout()
         layout.addWidget(self.title)
-        layout.addWidget(self.IPtext)
-        layout.addWidget(self.edit)
-        layout.addWidget(self.IPtext1)
-        layout.addWidget(self.edit1)
+        layout.addWidget(self.title2)
+        layout.addWidget(self.Fieldtext2)
+        layout.addWidget(self.FieldPacketAmount)
+        layout.addWidget(self.Fieldtext3)
+        layout.addWidget(self.FieldVariableName)
+        layout.addWidget(self.button1)
+        layout.addWidget(self.title1)
+        layout.addWidget(self.Fieldtext)
+        layout.addWidget(self.FieldIPAddress)
+        layout.addWidget(self.Fieldtext1)
+        layout.addWidget(self.FieldDuration)
         layout.addWidget(self.button)
         self.setLayout(layout)
-        self.button.clicked.connect(self.packet)
+        self.button.clicked.connect(self.echo)
+        self.button1.clicked.connect(self.sniff)
 
 
 
-    def packet(self):
-        print(f"Your IP Address is: {self.edit.text()}")
-        packetSniff.ICMPEchoRequest(self.edit.text(), self.edit1.text())
+    def echo(self):
+        print(f"Your IP Address is: {self.FieldIPAddress.text()}")
+        packetSniff.ICMPEchoRequest(self.FieldIPAddress.text(), self.FieldDuration.text())
+
+    def sniff(self):
+        print(f"Your packet count is: {self.FieldPacketAmount.text()}")
+        print(f"Your variable name is: {self.FieldVariableName.text()}")
+        packetSniff.Sniff(self.FieldPacketAmount.text(), self.FieldVariableName.text())
 
     
 
--- packetSniff.py
+++ packetSniff.py
@@ -1,6 +1,11 @@
 import sys
 from scapy.all import sr1,IP,ICMP
+from scapy.all import *
 
+
+def Sniff(count_var, var_name):
+    count_var = int(count_var)
+    sniff(count=count_var, prn=lambda var_name: var_name.summary())
 
 def ICMPEchoRequest(input_dst, time_var):
     if time_var == "" or input_dst == "":
